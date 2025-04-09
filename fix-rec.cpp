d7eddb992ca9c328581e6a42eb9d666630467640
rapidfire090 <glgavinli@gmail.com>
2025-04-09 15:28:49 -0400

    Update fix-rec.cpp

--- fix-rec.cpp
+++ fix-rec.cpp
@@ -1,4 +1,3 @@
-// fix_relay_tcp.cpp
 #include <iostream>
 #include <thread>
 #include <cstring>
@@ -8,14 +7,21 @@
 
 void relay(int client_sock, const char* out_ip, int out_port) {
     int forward_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (forward_sock < 0) {
+        perror("socket (forward)");
+        close(client_sock);
+        return;
+    }
+
     sockaddr_in forward_addr {};
     forward_addr.sin_family = AF_INET;
     forward_addr.sin_port = htons(out_port);
     inet_pton(AF_INET, out_ip, &forward_addr.sin_addr);
 
     if (connect(forward_sock, (sockaddr*)&forward_addr, sizeof(forward_addr)) < 0) {
-        perror("connect (outgoing)");
+        perror("connect (forward)");
         close(client_sock);
+        close(forward_sock);
         return;
     }
 
@@ -29,12 +35,22 @@ void relay(int client_sock, const char* out_ip, int out_port) {
     close(forward_sock);
 }
 
-int main() {
-    const int listen_port = 4000;
-    const char* forward_ip = "192.168.2.2"; // Next-hop IP
-    const int forward_port = 4001;
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <listen_port> <forward_ip> <forward_port>\n";
+        return 1;
+    }
+
+    int listen_port = std::stoi(argv[1]);
+    const char* forward_ip = argv[2];
+    int forward_port = std::stoi(argv[3]);
 
     int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock < 0) {
+        perror("socket (listen)");
+        return 1;
+    }
+
     int opt = 1;
     setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
 
@@ -43,14 +59,26 @@ int main() {
     listen_addr.sin_port = htons(listen_port);
     listen_addr.sin_addr.s_addr = INADDR_ANY;
 
-    bind(listen_sock, (sockaddr*)&listen_addr, sizeof(listen_addr));
-    listen(listen_sock, 10);
+    if (bind(listen_sock, (sockaddr*)&listen_addr, sizeof(listen_addr)) < 0) {
+        perror("bind");
+        return 1;
+    }
+
+    if (listen(listen_sock, 10) < 0) {
+        perror("listen");
+        return 1;
+    }
+
+    std::cout << "Listening on port " << listen_port << " and forwarding to "
+              << forward_ip << ":" << forward_port << std::endl;
 
     while (true) {
-        sockaddr_in client_addr;
+        sockaddr_in client_addr {};
         socklen_t addrlen = sizeof(client_addr);
         int client_sock = accept(listen_sock, (sockaddr*)&client_addr, &addrlen);
-        std::thread(relay, client_sock, forward_ip, forward_port).detach();
+        if (client_sock >= 0) {
+            std::thread(relay, client_sock, forward_ip, forward_port).detach();
+        }
     }
 
     close(listen_sock);
