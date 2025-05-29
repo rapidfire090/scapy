0218d97ad97e60cbc9c514dc47c3ddfaaec228e5
rapidfire090 <glgavinli@gmail.com>
2025-05-29 15:11:52 -0400

    Update fix_relay_v9.cpp

--- fix_relay_v9.cpp
+++ fix_relay_v9.cpp
@@ -14,8 +14,6 @@
 #include <sched.h>
 #include <sys/resource.h>
 
-// ---------------------- Message & LogEntry ----------------------
-
 struct Message {
     std::array<char, 1024> data;
     size_t length;
@@ -29,9 +27,8 @@ struct LogEntry {
     uint64_t latency_ns;
     uint64_t send_delta_ns;
     uint64_t total_latency_ns;
+    std::array<char, 32> clordid;
 };
-
-// ---------------------- Lock-Free SPSC Queue ----------------------
 
 template <typename T, size_t Capacity>
 class SPSCQueue {
@@ -59,8 +56,6 @@ public:
     }
 };
 
-// ---------------------- Globals ----------------------
-
 SPSCQueue<Message, 256> queue;
 SPSCQueue<LogEntry, 4096> log_queue;
 
@@ -69,7 +64,14 @@ std::atomic<int> debug_level{0};
 std::string log_file_path;
 int log_flush_interval_ms = 50;
 
-// ---------------------- Logging Thread ----------------------
+std::string extract_fix_tag11(const char* data, size_t len) {
+    std::string msg(data, len);
+    size_t pos = msg.find("11=");
+    if (pos == std::string::npos) return "";
+    size_t end = msg.find('\x01', pos);
+    if (end == std::string::npos) return msg.substr(pos + 3);
+    return msg.substr(pos + 3, end - pos - 3);
+}
 
 void log_writer_thread(const std::string& file_path, int flush_interval_ms) {
     sched_param param{};
@@ -90,13 +92,12 @@ void log_writer_thread(const std::string& file_path, int flush_interval_ms) {
                 << entry.recv_delta_ns << ","
                 << entry.latency_ns << ","
                 << entry.send_delta_ns << ","
-                << entry.total_latency_ns << "\n";
+                << entry.total_latency_ns << ","
+                << entry.clordid.data() << "\n";
         }
         out.flush();
     }
 }
-
-// ---------------------- Receive Thread ----------------------
 
 void recv_thread(int client_sock) {
     std::cout << "[recv] Thread started" << std::endl;
@@ -127,151 +128,4 @@ void recv_thread(int client_sock) {
 
     close(client_sock);
     std::cout << "[recv] Closed client socket" << std::endl;
-}
-
-// ---------------------- Send Thread ----------------------
-
-void send_thread(const char* forward_ip, int forward_port) {
-    std::cout << "[send] Connecting to " << forward_ip << ":" << forward_port << std::endl;
-
-    int forward_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (forward_sock < 0) {
-        perror("[send] socket");
-        return;
-    }
-
-    int flag = 1;
-    setsockopt(forward_sock, IPPROTO_TCP, TCP_NODELAY, &flag, sizeof(flag));
-
-    sockaddr_in forward_addr{};
-    forward_addr.sin_family = AF_INET;
-    forward_addr.sin_port = htons(forward_port);
-    inet_pton(AF_INET, forward_ip, &forward_addr.sin_addr);
-
-    if (connect(forward_sock, (sockaddr*)&forward_addr, sizeof(forward_addr)) < 0) {
-        perror("[send] connect");
-        close(forward_sock);
-        return;
-    }
-
-    std::cout << "[send] Connected" << std::endl;
-
-    Message msg;
-    while (true) {
-        int spin = 0;
-        while (!queue.pop(msg)) {
-            if (++spin > 1000) spin = 0;
-        }
-
-        uint64_t send_start_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(
-            std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-
-        ssize_t sent = send(forward_sock, msg.data.data(), msg.length, 0);
-
-        uint64_t send_end_ns = std::chrono::duration_cast<std::chrono::nanoseconds>(
-            std::chrono::high_resolution_clock::now().time_since_epoch()).count();
-
-        if (sent < 0) {
-            perror("[send] send");
-            break;
-        }
-
-        if (enable_latency && debug_level == 2) {
-            uint64_t recv_delta = msg.recv_end_ns - msg.recv_start_ns;
-            uint64_t latency = send_start_ns - msg.recv_end_ns;
-            uint64_t send_delta = send_end_ns - send_start_ns;
-            uint64_t total = recv_delta + latency + send_delta;
-            LogEntry entry{msg.recv_start_ns, recv_delta, latency, send_delta, total};
-            log_queue.push(entry);
-        }
-    }
-
-    close(forward_sock);
-    std::cout << "[send] Forward socket closed" << std::endl;
-}
-
-// ---------------------- Main ----------------------
-
-int main(int argc, char* argv[]) {
-    if (argc < 7) {
-        std::cerr << "Usage: " << argv[0]
-                  << " <listen_ip> <listen_port> <forward_ip> <forward_port> <rx_cpu> <tx_cpu> "
-                  << "[--measure-latency <log_file> <flush_interval_ms> [--debug-level=2]]" << std::endl;
-        return 1;
-    }
-
-    const char* listen_ip = argv[1];
-    int listen_port = std::stoi(argv[2]);
-    const char* forward_ip = argv[3];
-    int forward_port = std::stoi(argv[4]);
-    int rx_cpu = std::stoi(argv[5]);
-    int tx_cpu = std::stoi(argv[6]);
-
-    for (int i = 7; i < argc; ++i) {
-        std::string arg = argv[i];
-        if (arg == "--measure-latency" && i + 2 < argc) {
-            enable_latency = true;
-            log_file_path = argv[++i];
-            log_flush_interval_ms = std::stoi(argv[++i]);
-        } else if (arg.rfind("--debug-level=", 0) == 0) {
-            debug_level = std::stoi(arg.substr(14));
-        }
-    }
-
-    if (enable_latency && log_file_path.size() > 0) {
-        std::thread logger(log_writer_thread, log_file_path, log_flush_interval_ms);
-        logger.detach();
-    }
-
-    int listen_sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (listen_sock < 0) {
-        perror("[main] socket");
-        return 1;
-    }
-
-    int opt = 1;
-    setsockopt(listen_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
-
-    sockaddr_in listen_addr{};
-    listen_addr.sin_family = AF_INET;
-    listen_addr.sin_port = htons(listen_port);
-    inet_pton(AF_INET, listen_ip, &listen_addr.sin_addr);
-
-    if (bind(listen_sock, (sockaddr*)&listen_addr, sizeof(listen_addr)) < 0) {
-        perror("[main] bind");
-        return 1;
-    }
-
-    if (listen(listen_sock, 10) < 0) {
-        perror("[main] listen");
-        return 1;
-    }
-
-    std::cout << "[main] Listening on " << listen_ip << ":" << listen_port << std::endl;
-
-    while (true) {
-        sockaddr_in client_addr{};
-        socklen_t addrlen = sizeof(client_addr);
-        int client_sock = accept(listen_sock, (sockaddr*)&client_addr, &addrlen);
-        if (client_sock >= 0) {
-            std::cout << "[main] Accepted connection" << std::endl;
-
-            std::thread rx(recv_thread, client_sock);
-            cpu_set_t set_rx;
-            CPU_ZERO(&set_rx);
-            CPU_SET(rx_cpu, &set_rx);
-            pthread_setaffinity_np(rx.native_handle(), sizeof(cpu_set_t), &set_rx);
-            rx.detach();
-
-            std::thread tx(send_thread, forward_ip, forward_port);
-            cpu_set_t set_tx;
-            CPU_ZERO(&set_tx);
-            CPU_SET(tx_cpu, &set_tx);
-            pthread_setaffinity_np(tx.native_handle(), sizeof(cpu_set_t), &set_tx);
-            tx.detach();
-        }
-    }
-
-    close(listen_sock);
-    return 0;
 }
